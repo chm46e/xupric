68efea50c70d6477db0c97b1fb8dca2169ac03fc
chm46e <chm46e@duck.com>
2022-04-07 21:27:20 +0300

    fix: menu's logo installation
    The main menu's logo wasn't in the install script.
    
    Reviewed-by: chm46e <chm46e@duck.com>

--- src/sh/install.sh
+++ src/sh/install.sh
@@ -7,3 +7,4 @@ cp -f ../res/xupric.desktop /usr/share/applications/xupric.desktop
 
 cp -n ../res/icons/xupric_star_no.png /usr/share/icons/hicolor/scalable/apps/
 cp -n ../res/icons/xupric_star_yes.png /usr/share/icons/hicolor/scalable/apps/
+cp -n ../res/icons/xupric_main_menu.png /usr/share/icons/hicolor/scalable/apps/
